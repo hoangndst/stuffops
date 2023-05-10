e0d7f78ffbbb3197f9b74b18c65c82ad8bbc485b
hoangndst <hoangndst@gmail.com>
2023-05-10 19:57:42 +0000

    fix frontend

--- VDT/2. Ansible/NguyenDinhHoang/webapp/frontend/src/pages/Home.tsx
+++ VDT/2. Ansible/NguyenDinhHoang/webapp/frontend/src/pages/Home.tsx
@@ -3,10 +3,22 @@ import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import { Typography } from '@mui/material'
 import { format } from 'date-fns'
+import axios from 'axios'
 
 const Home = () => {
 
   const date = new Date()
+  const [ip, setIp] = React.useState('')
+
+  React.useEffect(() => {
+    axios.get('https://geolocation-db.com/json/')
+      .then((res) => {
+        setIp(res.data.IPv4)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
 
   return (
     <Box sx={{ flexGrow: 1, mt: 5 }}>
@@ -20,7 +32,7 @@ const Home = () => {
           <Typography
             sx={{ textAlign: 'center', fontSize: '2.8rem', fontWeight: 'bold' }}
           >
-            Welcome back!
+            Welcome back! The IP address is {ip}
           </Typography>
         </Grid>
       </Grid>
